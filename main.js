d1ce79dfd3ba3c5cd447b8fbe42ef521470b9913
LimitlessSocks <socktheringleader@gmail.com>
2025-11-30 19:34:32 -0500

    add connection brokering and lobby system

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ import { NaixtGameRenderer } from "./lib/render.js";
 import { NaixtGameEngine } from "./lib/engine.js";
 import { NaixtGameController } from "./lib/controller.js";
 import { NaixtPlayer } from "./lib/player.js";
-import { assertDefined, makeElement } from "./lib/lib.js";
+import { assert, assertDefined, makeElement } from "./lib/lib.js";
 import { Popup } from "./lib/popup.js";
 import { PeerConnection } from "./lib/peer.js";
 import cardsDatabase from "./data/cards.json" with { type: "json" };
@@ -18,20 +18,110 @@ window.addEventListener("load", function () {
     let controller = new NaixtGameController({ engine, renderer, pov });
     let gameArea = document.querySelector(".game-area");
 
-    let peer = null;
+    let peer = new PeerConnection();
     /**** main menu interface panel ****/
-    panelEngine.addButtonTransition(".solo-mode", "game-view", () => {
+    let lobbyPeers = document.querySelector(".lobby-peers");
+    panelEngine.addButtonTransition(".connect", "lobby-view", () => {
+        let peerIds = document.querySelectorAll(".my-peer-id");
+        lobbyPeers.textContent = "";
+        const refresh = () => {
+            lobbyPeers.textContent = "";
+            peer.getPeerList().then(list => {
+                list.forEach(id => {
+                    let connectButton = makeElement("button.connect-peer", "Connect");
+                    lobbyPeers.appendChild(makeElement("li", [
+                        connectButton,
+                        makeElement("span.their-peer-id", id),
+                    ]));
+                });
+                if(list.length === 0) {
+                    lobbyPeers.appendChild(makeElement("li", "(nothing to see here!)"));
+                }
+            });
+        };
+        if(!peer) {
+            peerIds.forEach(el => el.textContent = "Connecting...");
+        }
+        // TODO: advertise metadata about connection
+        peer ??= new PeerConnection();
+        peer.establishPeerId().then(id => {
+            peerIds.forEach(el => el.textContent = id);
+            refresh();
+            let waitingPopup, metadata;
+            peer.addEventListener("connectionRequest", ev => {
+                // TODO: queue multiple connection requests
+                metadata = ev.detail;
+                console.log("Connection details:", metadata);
+                waitingPopup = Popup.withTitleAndContent(
+                    "Incoming connection",
+                    `Accept connection from ${metadata.name}? (Peer id ${metadata.id})`,
+                    ["Accept", "Deny"],
+                );
+                waitingPopup.addEventListener("accept", ev => {
+                    let { name } = ev.detail;
+                    if(name === "Accept") {
+                        peer.acceptConnection(metadata.id);
+                        panelEngine.focus("game-view");
+                    }
+                    else {
+                        peer.rejectConnection(metadata.id);
+                    }
+                });
+                waitingPopup.addEventListener("cancel", () => peer.rejectConnection(metadata.id));
+                waitingPopup.deploy();
+            }, { once: true });
+            
+            peer.addEventListener("connectionRequestCancelled", ev => {
+                waitingPopup.kill();
+                Popup.withTitleAndContent("Request cancelled", `${metadata.name} (id ${metadata.id}) cancelled their request`, ["OK"])
+                    .deploy();
+            }, { once: true });
+        });
+        document.querySelector(".lobby-refresh").addEventListener("click", refresh);
+    });
+    // TODO: REMOVE; TESTING
+    document.querySelector(".connect").click();
+    /**** lobby interface panel ****/
+    lobbyPeers.addEventListener("click", ev => {
+        if(ev.target.classList.contains("connect-peer")) {
+            let peerId = ev.target.parentElement.querySelector(".their-peer-id");
+            assert(peerId, "No peer ID found");
+            peerId = peerId.textContent;
+            console.log(peerId);
+            // TODO: popup content to reply with user's name while waiting
+            let popup = Popup.withTitleAndContent("Waiting...", "No response yet", ["Cancel"]);
+            popup.addEventListener("accept", () => {
+                peer.cancelConnectionRequest(peerId);
+            });
+            popup.addEventListener("cancel", () => {
+                peer.cancelConnectionRequest(peerId);
+            });
+            popup.deploy();
+            peer.requestConnectionWithPeer(peerId)
+                .then(conn => {
+                    console.log("Successfully established connection", conn);
+                    popup.kill();
+                    panelEngine.focus("game-view");
+                })
+                .catch(err => {
+                    popup.kill();
+                    Popup.withTitleAndContent("Could not connect", `Reason: ${err.reason}`, ["OK"])
+                        .deploy();
+                });
+        }
+    });
+    /*panelEngine.addButtonTransition(".solo-mode", "game-view", () => {
         controller.initialize(gameArea);
         controller.render();
     });
-    // TODO: use peer discovery
-    // TODO: figure out how --cors works
-    // e.g. `peerjs --port 9007 --key peerjs --allow_discovery --cors=localhost:8080` doesn't work
+    */
+    // cors: peerjs --port 9007 --key peerjs --allow_discovery --cors=http://localhost:8080
+    /*
     panelEngine.addButtonTransition(".peer-host", "host-view", () => {
         let statusMessage = document.querySelector(".host-status");
         statusMessage.textContent = "Awaiting response from Peer server...";
         peer ??= new PeerConnection();
-        peer.host().then(id => {
+        peer.connect().then(id => {
             statusMessage.textContent = `Your id: ${id}`;
         });
     });
@@ -39,6 +129,7 @@ window.addEventListener("load", function () {
         let statusMessage = document.querySelector(".connect-status");
         statusMessage.textContent = "Please enter a Peer ID (doesn't work yet)";
     });
+    */
     panelEngine.addButtonTransition(".interface-options", "options-view", () => {
         optionsPeerServer.value = NaixtLocalStorage.get("peerServer");
         syncOptionsVisualState();
@@ -60,10 +151,10 @@ window.addEventListener("load", function () {
     panelEngine.addButtonTransition(".options-return", "interface-view");
 
     /**** host view panel ****/
-    panelEngine.addButtonTransition(".host-return", "interface-view");
+    // panelEngine.addButtonTransition(".host-return", "interface-view");
 
     /**** connect view panel ****/
-    panelEngine.addButtonTransition(".connect-return", "interface-view");
+    // panelEngine.addButtonTransition(".connect-return", "interface-view");
 
     /**** spectate view panel ****/
     // TODO
